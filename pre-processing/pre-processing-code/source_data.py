19ca5d65fbc4d11d0170617e1af906a6ef7def64
Masi Nazarian <masi000@gmail.com>
2020-11-22 19:53:56 -0800

    fix device out of disk space bug in lambda

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -6,7 +6,7 @@ from urllib.error import URLError, HTTPError
 from zipfile import ZipFile
 from s3_md5_compare import md5_compare
 from boto3.s3.transfer import TransferConfig
-
+from io import BytesIO
 
 def source_dataset():
     source_dataset_url = "https://data.cdc.gov/api/views/vbim-akqf/rows.csv"
@@ -38,36 +38,30 @@ def source_dataset():
 
     file_location = os.path.join(data_dir, data_set_name+'.csv')
 
-    with open(file_location, 'wb') as f:
-        f.write(response.read())
-
     s3_bucket = os.environ['S3_BUCKET']
     s3 = boto3.client('s3')
+    s3_resource = boto3.resource('s3')
     config = TransferConfig(multipart_threshold=1024*25, max_concurrency=10,
                             multipart_chunksize=1024*25, use_threads=True)
 
     s3_uploads = []
     asset_list = []
 
-    for r, d, f in os.walk(data_dir):
-        for filename in f:
-            obj_name = os.path.join(r, filename).split('/', 3).pop().replace(' ', '_').lower()
-            file_location = os.path.join(r, filename)
-            new_s3_key = data_set_name + '/dataset/' + obj_name
+    obj_name = file_location.split('/', 3).pop().replace(' ', '_').lower()
+    file_location = os.path.join(data_dir, obj_name)
+    new_s3_key = data_set_name + '/dataset/' + obj_name
+    filedata = response.read()
 
-            has_changes = md5_compare(s3, s3_bucket, new_s3_key, file_location)
-            if has_changes:
-                # s3.upload_file(file_location, s3_bucket, new_s3_key)
-                s3.upload_file(
-                    file_location, s3_bucket, new_s3_key,
-                    Config=config 
-                )
-                print('Uploaded: ' + filename)
-            else:
-                print('No changes in: ' + filename)
+    has_changes = md5_compare(s3, s3_bucket, new_s3_key, BytesIO(filedata))
+    if has_changes:
+        s3_resource.Object(s3_bucket, new_s3_key).put(Body=filedata)
+        sys.exit(0)
+        print('Uploaded: ' + file_location)
+    else:
+        print('No changes in: ' + file_location)
 
-            asset_source = {'Bucket': s3_bucket, 'Key': new_s3_key}
-            s3_uploads.append({'has_changes': has_changes, 'asset_source': asset_source})
+    asset_source = {'Bucket': s3_bucket, 'Key': new_s3_key}
+    s3_uploads.append({'has_changes': has_changes, 'asset_source': asset_source})
 
     count_updated_data = sum(upload['has_changes'] == True for upload in s3_uploads)
     if count_updated_data > 0:
